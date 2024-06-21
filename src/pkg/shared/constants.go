c94b3668fd47a567bfcf6d046ece331ad1746337
J Neo <jo.neoyj@gmail.com>
2024-06-21 22:36:26 +0800

    Working authentication

---
+++ src/pkg/shared/constants.go
@@ -0,0 +1,9 @@
+package shared
+
+const (
+	LOGS_DIR              = "logs"
+	IMAGE_PATH            = "image.png"
+	AUTHORIZED_USERS_FILE = "authorized_users.json"
+	WORKOUT_DATA_DIR      = "data"
+	WORKOUT_DATA_FILE     = "workout_data.json"
+)
